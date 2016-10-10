bbfb1bafbeb5e323c16f71d7fb42cd0f94225855
Graham Taylor <graham.taylor.90@gmail.com>
2016-10-10 19:08:52 +0000

    getting the app set up

--- node-project/app.js
+++ node-project/app.js
@@ -1,2 +1,16 @@
-var hello = "hello world";
-console.log(hello);
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT; //usually would use a number 
+
+app.get('/', function(req, res){
+    res.send('hello world!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('hello routing!');
+});
+
+app.listen(port, function(err){
+   console.log('the server is running on port: ' + port); 
+});
